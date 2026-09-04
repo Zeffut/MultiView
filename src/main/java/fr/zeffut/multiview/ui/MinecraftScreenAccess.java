22da6b7a280663e969c0b41b1024e20dfaa9ab01
Zeffut <74143023+Zeffut@users.noreply.github.com>
2026-09-04 23:25:56 +0200

    fix: preserve screen API invocation failures

--- src/main/java/fr/zeffut/multiview/ui/MinecraftScreenAccess.java
+++ src/main/java/fr/zeffut/multiview/ui/MinecraftScreenAccess.java
@@ -1,5 +1,7 @@
 package fr.zeffut.multiview.ui;
 
+import java.lang.reflect.InvocationTargetException;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 
@@ -13,26 +15,30 @@ public final class MinecraftScreenAccess {
     public static Screen getScreen(Minecraft minecraft) {
         try {
             return (Screen) Minecraft.class.getField("screen").get(minecraft);
-        } catch (ReflectiveOperationException legacyApiAbsent) {
+        } catch (NoSuchFieldException legacyApiAbsent) {
             try {
                 Object gui = Minecraft.class.getField("gui").get(minecraft);
                 return (Screen) gui.getClass().getMethod("screen").invoke(gui);
             } catch (ReflectiveOperationException modernApiAbsent) {
                 throw new IllegalStateException("Unable to access the current Minecraft screen", modernApiAbsent);
             }
+        } catch (IllegalAccessException legacyApiFailed) {
+            throw new IllegalStateException("Unable to access the current Minecraft screen", legacyApiFailed);
         }
     }
 
     public static void setScreen(Minecraft minecraft, Screen screen) {
         try {
             Minecraft.class.getMethod("setScreen", Screen.class).invoke(minecraft, screen);
-        } catch (ReflectiveOperationException legacyApiAbsent) {
+        } catch (NoSuchMethodException legacyApiAbsent) {
             try {
                 Object gui = Minecraft.class.getField("gui").get(minecraft);
                 gui.getClass().getMethod("setScreen", Screen.class).invoke(gui, screen);
             } catch (ReflectiveOperationException modernApiAbsent) {
                 throw new IllegalStateException("Unable to set the Minecraft screen", modernApiAbsent);
             }
+        } catch (IllegalAccessException | InvocationTargetException legacyApiFailed) {
+            throw new IllegalStateException("Unable to set the Minecraft screen", legacyApiFailed);
         }
     }
 }
